88f1e06a262a6f05bfafcc1ffe9584a8043672c0
Paulo Roberto Vieira Ribeiro Cavalcanti <paulor.vrcavalcanti@gmail.com>
2023-01-21 09:45:45 -0400

    First commit

---
+++ src/main/java/br/dev/pauloroberto/gerenciamentopessoas/ApiGerenciamentoPessoasApplication.java
@@ -0,0 +1,13 @@
+package br.dev.pauloroberto.gerenciamentopessoas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiGerenciamentoPessoasApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiGerenciamentoPessoasApplication.class, args);
+    }
+
+}
