449a17c89718a5987a4b60466c5872796632b97f
Paulo Roberto Vieira Ribeiro Cavalcanti <paulor.vrcavalcanti@gmail.com>
2023-01-23 22:35:09 -0300

    Adicionadas anotações, um novo construtor e novos métodos

--- src/main/java/br/dev/pauloroberto/gerenciamentopessoas/domain/model/Endereco.java
+++ src/main/java/br/dev/pauloroberto/gerenciamentopessoas/domain/model/Endereco.java
@@ -1,7 +1,11 @@
 package br.dev.pauloroberto.gerenciamentopessoas.domain.model;
 
+import br.dev.pauloroberto.gerenciamentopessoas.api.model.EnderecoDto;
+import br.dev.pauloroberto.gerenciamentopessoas.domain.ValidationGroups;
 import jakarta.persistence.*;
+import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.groups.ConvertGroup;
 import lombok.*;
 
 @Getter
@@ -9,11 +13,13 @@ import lombok.*;
 @AllArgsConstructor
 @NoArgsConstructor
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@ToString
 @Entity
 public class Endereco {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @EqualsAndHashCode.Include
+    @NotNull(groups = ValidationGroups.EnderecoId.class)
     private Long id;
     @NotNull
     private String logradouro;
@@ -25,8 +31,28 @@ public class Endereco {
     private String estado;
     @NotNull
     private String cep;
-    @NotNull
-    private boolean principal;
     @ManyToOne
+    @NotNull
+    @Valid
+    @ConvertGroup(to = ValidationGroups.PessoaId.class)
     private Pessoa pessoa;
+    private boolean principal;
+
+    public Endereco(EnderecoDto endereco, Pessoa pessoa) {
+        this.logradouro = endereco.logradouro();
+        this.numero = endereco.numero();
+        this.cidade = endereco.cidade();
+        this.estado = endereco.estado();
+        this.cep = endereco.cep();
+        this.pessoa = pessoa;
+    }
+
+    public void setPrincipal() {
+        this.principal = true;
+        this.pessoa.setEnderecoPrincipal(this.id);
+    }
+
+    public void unsetPrincipal() {
+        this.principal = false;
+    }
 }
