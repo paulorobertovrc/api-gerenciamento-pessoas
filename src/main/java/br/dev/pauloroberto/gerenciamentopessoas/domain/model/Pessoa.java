ae3c2ce813133e7ba85c01b6c6db61a7a654aa3c
Paulo Roberto Vieira Ribeiro Cavalcanti <paulor.vrcavalcanti@gmail.com>
2023-01-23 22:35:30 -0300

    Adicionadas anotações e novos métodos

--- src/main/java/br/dev/pauloroberto/gerenciamentopessoas/domain/model/Pessoa.java
+++ src/main/java/br/dev/pauloroberto/gerenciamentopessoas/domain/model/Pessoa.java
@@ -1,24 +1,36 @@
 package br.dev.pauloroberto.gerenciamentopessoas.domain.model;
 
-import jakarta.persistence.*;
+import br.dev.pauloroberto.gerenciamentopessoas.api.model.PessoaDto;
+import br.dev.pauloroberto.gerenciamentopessoas.domain.ValidationGroups;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotNull;
 import lombok.*;
 
 import java.time.LocalDate;
-import java.util.List;
 
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@ToString
 @Entity
 public class Pessoa {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @EqualsAndHashCode.Include
+    @NotNull(groups = ValidationGroups.PessoaId.class)
     private Long id;
     private String nome;
-    private LocalDate dataNascimento;
-    @OneToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
-    private List<Endereco> endereco;
+    private LocalDate nascimento;
+    private Long enderecoPrincipal;
+
+    public Pessoa(PessoaDto pessoaDto) {
+        this.nome = pessoaDto.nome();
+        this.nascimento = LocalDate.parse(pessoaDto.nascimento());
+        this.enderecoPrincipal = null;
+    }
 }
