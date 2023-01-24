31c4e0eced0c165a2d98c7c6b1adfe4e793eb243
Paulo Roberto Vieira Ribeiro Cavalcanti <paulor.vrcavalcanti@gmail.com>
2023-01-23 22:36:24 -0300

    Adicionado método findAllByPessoaId

--- src/main/java/br/dev/pauloroberto/gerenciamentopessoas/domain/repository/EnderecoRepository.java
+++ src/main/java/br/dev/pauloroberto/gerenciamentopessoas/domain/repository/EnderecoRepository.java
@@ -4,9 +4,12 @@ import br.dev.pauloroberto.gerenciamentopessoas.domain.model.Endereco;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface EnderecoRepository extends CrudRepository<Endereco, Long> {
     Optional<Object> findByPessoaId(Long id);
+
+    List<Endereco> findAllByPessoaId(Long pessoaId);
 }
